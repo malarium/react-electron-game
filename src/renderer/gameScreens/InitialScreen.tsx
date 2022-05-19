df248c11df71592e86101f3d230689f9652b1f41
Marcin <marcingorecki1981@gmail.com>
2022-05-19 18:56:26 +0200

    images for background to use areamap

--- src/renderer/gameScreens/InitialScreen.tsx
+++ src/renderer/gameScreens/InitialScreen.tsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
 import './InitialScreen.scss';
+import kitchen from '../../../assets/images/kitchen.jpg';
 
 export const InitialScreen = () => {
   return (
     <div className="hall">
-      <div className="Title">Initial screen</div>
-      <Link className="startGame" to="/">
+      <img className="backgroundImg" src={kitchen} width="100%" height="100%" />
+      {/* <Link className="startGame" to="/">
         BACK
-      </Link>
+      </Link> */}
     </div>
   );
 };
