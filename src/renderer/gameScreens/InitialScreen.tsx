af52b40a40677aa05640e37da8ca4a43eca10f96
Marcin <marcingorecki1981@gmail.com>
2022-05-19 18:44:36 +0200

    images

--- src/renderer/gameScreens/InitialScreen.tsx
+++ src/renderer/gameScreens/InitialScreen.tsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
+import './InitialScreen.scss';
 
 export const InitialScreen = () => {
   return (
-    <>
+    <div className="hall">
       <div className="Title">Initial screen</div>
       <Link className="startGame" to="/">
         BACK
       </Link>
-    </>
+    </div>
   );
 };
